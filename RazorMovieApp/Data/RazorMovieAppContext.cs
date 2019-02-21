5cc8951b9f11d9ac74e0d5761908f25db2adc42f
trhoads <35459520+trhoads@users.noreply.github.com>
2019-02-21 14:20:22 -0500

    Did Lesson 7 Tutorial

---
+++ RazorMovieApp/Data/RazorMovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorMovieApp.Models
+{
+    public class RazorMovieAppContext : DbContext
+    {
+        public RazorMovieAppContext (DbContextOptions<RazorMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
